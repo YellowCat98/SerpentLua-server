2adcd9e93ebe21ff038cdc26e394a8ca9f8fce01
YellowCat98 <elcucuyisgay@gmail.com>
2026-05-03 11:46:35 +0100

    ok i dfixed that

--- src/routes/plugin/add.js
+++ src/routes/plugin/add.js
@@ -5,6 +5,6 @@ export function entry(request, env, ctx) {
 	const url = new URL(request.url);
 	const params = url.searchParams; // using query parameters for this one
 
-	const key = params.get("sex")
+	const sex = params.get("sex")
 	return new Response(`yoyo: ${sex}`);
 }
