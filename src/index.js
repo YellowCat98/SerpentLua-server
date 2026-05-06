438b852327e6706b7bcc4e40ee0bdfc14796d2d9
YellowCat98 <elcucuyisgay@gmail.com>
2026-05-06 13:42:58 +0100

    added rate limiting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ export default {
     async fetch(request, env, ctx) {
         const { pathname } = new URL(request.url);
 
+        const ip = request.headers.get("CF-Connecting-IP") ?? "127.0.0.1";
+
+        const { success } = await env.RATE_LIMITER.limit({ key: ip });
+
+        if (!success) {
+            return new Response("Too Many Requests", { status: 429 });
+        }
+
         if (pathname.startsWith("/api")) return api(request, env, ctx);
 
         return new Response("Nothing but us chickens!", { status: 404 });
